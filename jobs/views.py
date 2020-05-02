09e40310dc15d1baaaf82a11bd600d26ceb0045f
vaibhav agarwal <agarwalvaibhav041@gmail.com>
2020-05-02 16:30:43 +0530

    first commit

---
+++ jobs/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import job
+def home(request):
+    jobs=job.objects
+    return render(request,'home.html',{'jobs':jobs})
---
+++ vaibhav2/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static  import static
+import jobs.views
+urlpatterns = [
+    path('',jobs.views.home),
+    path('admin/',admin.site.urls),
+    path('blog/',include('blogs.urls'))
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
