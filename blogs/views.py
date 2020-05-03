82999cbbb4dac0878b1ad0d75b825ba8c876a729
vaibhav agarwal <agarwalvaibhav041@gmail.com>
2020-05-03 17:12:42 +0530

    tes

--- blogs/views.py
+++ blogs/views.py
@@ -3,7 +3,6 @@ from .models import Blog
 def allblogs(request):
     blogs=Blog.objects
     return render(request,'blogs/allblogs.html',{'blogs':blogs})
-
 def detail(request,blog_id):
     detailblog=get_object_or_404(Blog,pk=blog_id)
     return render(request,'blogs/detail.html',{'blog':detailblog})
