f69deb69df5e5e8961da482bbac34a5163a02319
vaibhav agarwal <agarwalvaibhav041@gmail.com>
2020-05-03 17:06:19 +0530

    tes

--- blogs/views.py
+++ blogs/views.py
@@ -7,5 +7,3 @@ def allblogs(request):
 def detail(request,blog_id):
     detailblog=get_object_or_404(Blog,pk=blog_id)
     return render(request,'blogs/detail.html',{'blog':detailblog})
-    
-# Create your views here.
