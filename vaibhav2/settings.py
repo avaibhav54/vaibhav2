36fdaf1e73b72d211e509acb17f29ba7faaa2480
vaibhav agarwal <agarwalvaibhav041@gmail.com>
2020-05-03 11:25:37 +0530

    static

--- vaibhav2/settings.py
+++ vaibhav2/settings.py
@@ -124,11 +124,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'vaibhav2/static/')
 
+]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
 #>psql -U postgres postgres
 # Activate Django-Heroku.
 django_heroku.settings(locals())
